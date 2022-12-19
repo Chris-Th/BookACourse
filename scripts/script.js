2c22d13fd02200510a2d9742ce049a4c651543a8
Chris-Th <muhmuhmiau@proton.me>
2022-12-19 14:47:44 +0100

    start JS

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+function validateForm() {
+    let fName = document.getElementById("fName");
+    let lName = document.getElementById("lName")
+    let street = document.getElementById("street")
+    let streetNr = document.getElementById("streetNr")
+    let plz = document.getElementById("plz")
+    let ort = document.getElementById("ort")
+    let telG = document.getElementById("telG")
+    let telP = document.getElementById("telP")
+    let email = document.getElementById("email")
+    let birthdate = document.getElementById("birthdate")
+    let agbCheck = document.getElementById("agbCheck")
+    let vers = document.getElementById("vers")
+}
