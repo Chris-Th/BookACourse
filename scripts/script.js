0c6b956f9982b61e65349c749b52cac9b9e2893e
Chris-Th <muhmuhmiau@proton.me>
2022-12-20 12:26:53 +0100

    add flow chart

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,27 @@
-function validateForm() {
+/*function validateForm() {
     let fName = document.getElementById("fName");
-    let lName = document.getElementById("lName")
-    let street = document.getElementById("street")
-    let streetNr = document.getElementById("streetNr")
-    let plz = document.getElementById("plz")
-    let ort = document.getElementById("ort")
-    let telG = document.getElementById("telG")
-    let telP = document.getElementById("telP")
-    let email = document.getElementById("email")
-    let birthdate = document.getElementById("birthdate")
-    let agbCheck = document.getElementById("agbCheck")
-    let vers = document.getElementById("vers")
+    let lName = document.getElementById("lName");
+    let street = document.getElementById("street");
+    let streetNr = document.getElementById("streetNr");
+    let plz = document.getElementById("plz");
+    let ort = document.getElementById("ort");
+    let telG = document.getElementById("telG");
+    let telP = document.getElementById("telP");
+    let email = document.getElementById("email");
+    let birthdate = document.getElementById("birthdate");
+    let agbCheck = document.getElementById("agbCheck");
+    let versJa = document.getElementById("versJa");
+    let versNein = document.getElementById("versNein"); 
+}*/
+
+function bookingForm() {
+    // return validFName(); validLName(); validStreet(); etc.
 }
+    var fName = document.getElementById("fName").value
+
+    function validFName(fName) {
+        if (fName.value.length == 0) {
+            alert("Vorname ist Pflichtfeld");
+            return false;
+        }
+    }
